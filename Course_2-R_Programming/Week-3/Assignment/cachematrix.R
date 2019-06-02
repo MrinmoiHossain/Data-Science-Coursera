a2e7e41a95ea737b2a1edf30b157e0795632ea19
MrinmoiHossain <mokit.aust@gmail.com>
2019-06-02 06:29:53 +0600

    week-3 of r programming

---
+++ Course_2-R_Programming/Week-3/Assignment/cachematrix.R
@@ -0,0 +1,52 @@
+# Creates a special matrix object that can cache its inverse
+# Argument parameter: Matrix, Output: List
+makeCacheMatrix <- function(x = matrix()){
+    # Initialize the inverse value
+    m <- NULL
+    
+    # Method to set the matrix
+    set <- function(y){
+        x <<- y
+        m <<- NULL
+    }
+    
+    # Method to get the matrix
+    get <- function() x
+    
+    # Method to set the inverse of the matrix
+    setinverse <- function(inverse) m <<- inverse
+    
+    # Method to get the inverse of the matrix
+    getinverse <- function() m
+    
+    # Output list
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+}
+
+
+# To compute the inverse of the special matrix
+# Argument parameter: Matrix, Output: Inverse Matrix
+cachemean <- function(x, ...){
+    # Initialize a matrix that is the inverse of x matrix
+    m <- x$getinverse()
+    
+    # Return a matrix if it is the inverse of x matrix
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    
+    # Get the matrix from the object
+    data <- x$get()
+    
+    # Method to solve the inverse using matrix multiplication
+    m <- solve(data, ...)
+    
+    # Set the inverse of inverse matrix
+    x$setinverse(m)
+    
+    # Return the matrix
+    m
+}
